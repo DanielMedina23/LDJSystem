6479ac4b2b6e4fa720fee2246d38069899df6eb1
juap35824-lang <juap35824@gmail.com>
2026-09-02 19:17:53 +0200

    feat: agrego modelo de usuarios

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,18 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 
-class Empleado(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Usuario(AbstractUser):
     dni = models.CharField(max_length=9, unique=True)
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True, unique=True)
 
     def __str__(self):
         return self.user.username
-
-
-class Cliente(models.Model):
-    nombre = models.CharField(max_length=100)
-    telefono = models.CharField(max_length=20, blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
-    
-    def __str__(self):
-        return self.nombre
