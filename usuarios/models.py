f23f21729dd445340b0d00ae053d3793c9bdf2e2
juap35824-lang <juap35824@gmail.com>
2026-09-02 18:32:14 +0200

    feat: agrego modelo de usuarios

---
+++ usuarios/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Empleado(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dni = models.CharField(max_length=9, unique=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.nombre
