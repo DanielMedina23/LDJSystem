c093216c9f993e527e0658536f32df09c22113f4
juap35824-lang <juap35824@gmail.com>
2026-09-02 20:55:49 +0200

    feat: agrego crod viws, urls  y cambio del setings

---
+++ negocio/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.ver_negocio, name='ver_negocio'),
+
+    path(
+        'crear/',
+        views.crear_negocio,
+        name='crear_negocio'
+    ),
+
+    path(
+        'editar/<int:id>/',
+        views.editar_negocio,
+        name='editar_negocio'
+    ),
+
+    path(
+        'eliminar/<int:id>/',
+        views.eliminar_negocio,
+        name='eliminar_negocio'
+    ),
+]
--- negocio/views.py
+++ negocio/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Negocio
+from .forms import NegocioForm
 
 # Create your views here.
+def ver_negocio(request):
+    negocio = Negocio.objects.first()
+
+    return render(
+        request,
+        'negocio/negocio.html',
+        {'negocio': negocio}
+    )
+
+def crear_negocio(request):
+
+    if request.method == 'POST':
+
+        negocio_form = NegocioForm(request.POST)
+
+        if negocio_form.is_valid():
+            negocio_form.save()
+
+            return redirect('ver_negocio')
+    else:
+        negocio_form = NegocioForm()
+
+    return render(
+        request,
+        'negocio/crear.html',
+        {'negocio_form': negocio_form}
+    )
+
+
+def editar_negocio(request, id):
+
+    negocio = get_object_or_404(Negocio, id=id)
+
+    if request.method == 'POST':
+
+        negocio_form = NegocioForm(
+            request.POST,
+            instance=negocio
+        )
+
+        if negocio_form.is_valid():
+            negocio_form.save()
+
+            return redirect('ver_negocio')
+
+    else:
+        negocio_form = NegocioForm(instance=negocio)
+
+    return render(
+        request,
+        'negocio/editar.html',
+        {'negocio_form': negocio_form}
+    )
+
+def eliminar_negocio(request, id):
+
+    negocio = get_object_or_404(Negocio, id=id)
+
+    if request.method == 'POST':
+
+        negocio.delete()
+
+        return redirect('crear_negocio')
+
+    return render(
+        request,
+        'negocio/eliminar.html',
+        {'negocio': negocio}
+    )
