c561e2f5e125b15abdb70e585e8174effe5045f9
juap35824-lang <juap35824@gmail.com>
2026-09-03 19:48:58 +0200

    feat: agrego cambios en la app negocio, forms, models, vieus y  urls

--- negocio/forms.py
+++ negocio/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Negocio
+from .models import Negocio, Mesa, Horario
 
 
 class NegocioForm(forms.ModelForm):
@@ -10,4 +10,22 @@ class NegocioForm(forms.ModelForm):
             'nombre',
             'direccion',
             'activo',
+        ]
+
+class MesaForm(forms.ModelForm):
+    class Meta:
+        model = Mesa
+        fields = [
+            'nombre_interno',
+            'capacidad',
+            'activa',
+        ]
+
+class HorarioForm(forms.ModelForm):
+    class Meta:
+        model = Horario
+        fields = [
+            'dia_semana',
+            'hora_apertura',
+            'hora_cierre',
         ]
